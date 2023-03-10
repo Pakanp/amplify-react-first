b7b48bfff38173ed08b8ea3ddc7ef5108161ef8e
Pakanp <pakan.prsd@gmail.com>
2023-03-10 00:23:22 -0800

    clusters and infow windows

--- src/App.js
+++ src/App.js
@@ -1,16 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, Marker, InfoWindow, MarkerClusterer } from "@react-google-maps/api";
 
-function App() {
+function Map() {
+  const [map, setMap] = useState(null);
+  const [reports, setReports] = useState([]);
+  const [openMarkers, setOpenMarkers] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the external API
+    fetch('https://y16bh31ap4.execute-api.us-east-1.amazonaws.com/dev/reports')
+      .then(response => response.json())
+      .then(data => {
+        setReports(JSON.parse(data.body).Items);
+      });
+  }, []);
+
+  const markers = reports.map(item => {
+    return {
+      lat: parseFloat(item.Geo_Latitude.S),
+      lng: parseFloat(item.Geo_Longitude.S),
+      label: item.Tags.S,
+      id: item.Report_ID.S,
+      text: item.Report_Text.S
+    };
+  });
+
+  const onLoad = map => {
+    setMap(map);
+  };
+
+  const onMarkerClick = (marker, map) => {
+    setOpenMarkers(prevMarkers => [...prevMarkers, marker]);
+    console.log(marker);
+
+    if (marker.clusterer) {
+      const cluster = marker.clusterer.getMarkers();
+      const bounds = new window.google.maps.LatLngBounds();
+      cluster.forEach(marker => bounds.extend(marker.getPosition()));
+      const center = bounds.getCenter();
+      const zoom = map.getZoom() + 1;
+  
+      // Use the animateTo method to smoothly zoom into the cluster
+      map.animateTo({
+        center: center,
+        zoom: zoom,
+      });
+    }
+  };
+
+  const onCloseInfoWindow = marker => {
+    setOpenMarkers(prevMarkers => prevMarkers.filter(m => m !== marker));
+  };
+
+  const containerStyle = {
+    width: "100%",
+    height: "950px",
+  };
+
+  const [initialCenter, setInitialCenter] = useState({
+    lat: 37.6565, 
+    lng: -122.0552673
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <GoogleMap mapContainerStyle={containerStyle} center={initialCenter} zoom={16.5} onLoad={onLoad}>
+      <MarkerClusterer gridSize={50} maxZoom={20}>
+        {(clusterer) =>
+            markers.map((marker) => (
+              <Marker
+                key={marker.id}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                onClick={() => onMarkerClick(marker, map)}
+                clusterer={clusterer}
+                text={marker.label}
+                label={marker.label}
+              />
+            ))
+          }
+      </MarkerClusterer>
+      {openMarkers.map((marker, index) => (
+        <InfoWindow 
+          key={marker.id}
+          position={{lat: marker.lat, lng: marker.lng}} 
+          onCloseClick={() => onCloseInfoWindow(marker)}
+        >
+          <div>{marker.text}</div>
+        </InfoWindow>
+      ))}
+    </GoogleMap>
   );
 }
 
-export default App;
+export default Map;
---
+++ src/MapComponent.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+// import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+// class MapComponent extends Component {
+//   render() {
+//     const Map = withGoogleMap(props => (
+//       <GoogleMap
+//         defaultZoom={8}
+//         defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
+//       >
+//         <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
+//       </GoogleMap>
+//     ));
+
+//     return (
+//       <div style={{ height: '500px' }}>
+//         <Map
+//           containerElement={<div style={{ height: '100%' }} />}
+//           mapElement={<div style={{ height: '100%' }} />}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default MapComponent;
